2206d95d284a997323c313d30a025fea92b097b7
Ru-Rom <38590078+Ru-Rom@users.noreply.github.com>
2019-03-11 13:11:42 +0300

    Improved readability

--- RomanRudenkoLesson2/app.js
+++ RomanRudenkoLesson2/app.js
@@ -13,15 +13,21 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
     terminal: false
-  });
+});
 
 console.log('Я загадаю число, где 1 - орел, а 2 - решка. Попытайся отгадать что я загадал!');
 // 1 Необходимо рандомно загадывать значение 1 или 2
 const randomize = () => {
     return Math.floor(Math.random() * 2 + 1);
-}
+};
 // 2 Нужно проверять введеное пользователем значение с загаданным
 rl.on('line', function(line){
     line = parseInt(line);
-    (line === 1 || line === 2) ? (line === randomize() ? console.log('Вы угадали!') : console.log('Вы ошиблись')) : console.log('Введите правильное значение!');
-  })
+
+    (line === 1 || line === 2) ? (
+        line === randomize() ? 
+            console.log('Вы угадали!') 
+            : console.log('Вы ошиблись')
+    ) 
+        : console.log('Введите правильное значение!');
+});
